3e1dee6af28443d8a0ab8968f789e608b42f72ba
ludovdim <ludovdim@gmail.com>
2022-02-18 22:24:04 +0300

    0

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        // TODO: 18.02.2022 1
-        // TODO: 18.02.2022 2
-        // TODO: 18.02.2022 3 master
-        // TODO: 18.02.2022 3 master
-        // TODO: 18.02.2022 3 master
-        // TODO: 18.02.2022 3
-        // TODO: 18.02.2022 one 4
-        // TODO: 18.02.2022 one 4
-        // TODO: 18.02.2022 one 4
-        // TODO: 18.02.2022 one 4
-        // TODO: 18.02.2022 4
-        // TODO: 18.02.2022 4
-        // TODO: 18.02.2022 4
-        // TODO: 18.02.2022 second 1
-        // TODO: 18.02.2022 second 1
-        // TODO: 18.02.2022 second 1
-        // TODO: 18.02.2022 second 2
-        // TODO: 18.02.2022 subsec 1
-        // TODO: 18.02.2022 subs
-        // TODO: 18.02.2022 subs
-        //  ec 1
-        // TODO: 18.02.2022 subsec 1
-        // TODO: 18.02.2022 third 1
-        // TODO: 18.02.2022 third 2
-        // TODO: 18.02.2022 subthird 1
-        // TODO: 18.02.2022 subthird 1
-
     }
 
-
-
-    private void master(){
-// TODO: 18.02.2022 1
-// TODO: 18.02.2022 2
-// TODO: 18.02.2022 2
-// TODO: 18.02.2022 2
-// TODO: 18.02.2022 2
-    }
 
 }
